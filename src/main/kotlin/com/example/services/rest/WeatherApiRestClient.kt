f85161d05fc6f7fbcdb04a50b28393db2d6f70d3
richard <richardfabianbusca@gmail.com>
2024-10-12 16:50:24 -0300

    add simulate error

--- src/main/kotlin/com/example/services/rest/WeatherApiRestClient.kt
+++ src/main/kotlin/com/example/services/rest/WeatherApiRestClient.kt
@@ -5,15 +5,19 @@ import io.ktor.client.*
 import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.server.config.*
 import kotlinx.coroutines.delay
 import kotlinx.serialization.json.Json
+import kotlin.random.Random
 
 const val TIMEOUT = "rest.client.timeout"
 const val BASE_URL = "rest.client.base_url"
 const val API_KEY = "rest.client.api_key"
-private const val MAX_RETRIES = "rest.client.retries"
+const val MAX_RETRIES = "rest.client.retries"
 const val QUERY_PARAMS = "rest.client.query_params"
+
+private const val SIMULATION_ERROR_PROBABILITY = 20
 
 /**
  * REST client for interacting with the Weather API.
@@ -36,7 +40,7 @@ class WeatherApiRestClient(private val config: ApplicationConfig,private val cli
      * @throws Exception if there is an error during the API call.
      */
     suspend fun getWeatherByLocation(location: String): WeatherResponse {
-        val response: HttpResponse = client.get(config.property(BASE_URL).getString()){
+        val response = client.get(config.property(BASE_URL).getString()){
 
             // Delay of 500 milliseconds between API calls is implemented to comply with the API's rate limit,
             // which allows a maximum of 3 requests per second.
@@ -56,5 +60,18 @@ class WeatherApiRestClient(private val config: ApplicationConfig,private val cli
         }
 
         return Json.decodeFromString(response.bodyAsText())
+    }
+
+    /**
+     * Simulates an error based on a random value.
+     * Throws a RuntimeException if a randomly generated value
+     * is below a certain threshold.
+     */
+    private fun simulateError() {
+        val randomValue = Random.nextInt(1, 101)
+        val errorThreshold = SIMULATION_ERROR_PROBABILITY
+        if (randomValue <= errorThreshold) {
+            throw RuntimeException("Simulated error: Random value $randomValue triggered an exception.")
+        }
     }
 }
--- src/main/kotlin/com/example/tasks/WeatherScheduledTask.kt
+++ src/main/kotlin/com/example/tasks/WeatherScheduledTask.kt
@@ -3,9 +3,11 @@ package com.example.tasks
 import com.example.model.enums.GeographicLocation
 import com.example.services.WeatherService
 import com.example.services.redis.RedisService
+import com.example.services.rest.MAX_RETRIES
 import io.ktor.server.config.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -37,7 +39,9 @@ class WeatherScheduledTask(private val config: ApplicationConfig,
             GeographicLocation.entries.forEach { location ->
                 try {
                     println("Refreshing cache for $location")
-                    val weatherResponse = weatherService.getWeatherByLocation(location)
+                    val weatherResponse = retry(config.property(MAX_RETRIES).getString().toInt()) {
+                        weatherService.getWeatherByLocation(location)
+                    }
                     if (Objects.nonNull(weatherResponse)) {
                         redisService.set(location.name, Json.encodeToString(weatherResponse))
                     }
@@ -51,4 +55,18 @@ class WeatherScheduledTask(private val config: ApplicationConfig,
     override fun getDelay(): Long = config.property("tasks.weather.delay").getString().toLong()
 
     override fun getPeriod(): Long = config.property("tasks.weather.period").getString().toLong()
+
+    suspend fun <T> retry(times: Int, block: suspend () -> T): T {
+        var lastException: Exception? = null
+        repeat(times) { attempt ->
+            try {
+                return block()
+            } catch (e: Exception) {
+                lastException = e
+                println("Attempt ${attempt + 1} failed: ${e.message}. Retrying...")
+                delay(500)
+            }
+        }
+        throw lastException ?: Exception("Retry limit exceeded")
+    }
 }
